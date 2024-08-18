cb1d13da4e5cbaffbe9f515e6d365d20f7bcf7d1
Aurangzaib BALOCH <zaibbuzdar3@gmail.com>
2024-08-18 02:43:43 -0700

    commit

--- app/src/main/java/com/example/expensemanager/ui/theme/CustomColors.kt
+++ app/src/main/java/com/example/expensemanager/ui/theme/CustomColors.kt
@@ -3,9 +3,6 @@ package com.example.expensemanager.ui.theme
 import androidx.compose.ui.graphics.Color
 
 object CustomColors {
-    val PrimaryColor = Color(0xFFC197FD)
-    val BackgroundColor = Color(0xFF3F51B5)
-    val SecondaryColor = Color(0xFF24A79A)
     val mainCardColor = Color(0xFF46A8F7)
     val dimCardColor = Color(0xFF67B3F0)
     val incomeColor = Color(0x9E087944)
