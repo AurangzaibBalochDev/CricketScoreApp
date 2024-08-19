908567a2a5c9a76e60da93650e6aa3c33e81b02a
Aurangzaib BALOCH <zaibbuzdar3@gmail.com>
2024-08-19 05:12:58 -0700

    commit

--- app/src/main/java/com/example/expensemanager/presentation/expense_screen/ExpenseScreen.kt
+++ app/src/main/java/com/example/expensemanager/presentation/expense_screen/ExpenseScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,21 +32,16 @@ import com.example.expensemanager.presentation.expense_card.ExpenseCard
 import com.example.expensemanager.presentation.main_screen.components.TopBar
 import com.example.expensemanager.ui.theme.CustomColors
 
+
+@Preview
 @Composable
 fun ExpenseScreen(
-    navController: NavController,
-    viewModel: MainViewModel = hiltViewModel()
+//    navController: NavController,
+//    viewModel: MainViewModel = hiltViewModel()
 ) {
-    val mainState = viewModel.state.collectAsState().value
+//    val mainState = viewModel.state.collectAsState().value
     Scaffold(
-        modifier = Modifier.fillMaxSize().padding(vertical = 25.dp), topBar = { TopBar() },
-        floatingActionButton = {
-            FloatingActionButton(modifier = Modifier.padding(bottom = 0.dp), onClick = {
-                navController.navigate("${Screens.AddExpenseScreen.route}/${-1}")
-            }) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
-            }
-        },
+
     ) {
         Column(
             modifier = Modifier
@@ -78,10 +74,7 @@ fun ExpenseScreen(
                         verticalAlignment = Alignment.Bottom,
                         horizontalArrangement = Arrangement.Center,
                     ) {
-
-//                        Text(text = mainState.totalIncome.toString(), color = Color.Green)
-//                        Text(text = mainState.totalExpense.toString(), color = Color.Red)
-                        Text(text = mainState.totalBalance.toString(), fontSize = 40.sp)
+                        Text(text = "150", fontSize = 40.sp)
                     }
 
                 }
@@ -94,11 +87,11 @@ fun ExpenseScreen(
                     Row(
                         modifier = Modifier
                     ) {
-                        Text(text = "Income")
+                        Text(text = "Player 1")
                     }
                     Row(
                     ) {
-                        Text(text = "Expenses")
+                        Text(text = "Player 2")
                     }
                 }
                 Row(
@@ -110,30 +103,18 @@ fun ExpenseScreen(
                     Row(
                         modifier = Modifier
                     ) {
-                        Text(text = mainState.totalIncome.toString(), fontSize = 20.sp)
+                        Text(text = "50*", fontSize = 20.sp)
                     }
                     Row(
                     ) {
-                        Text(text = mainState.totalExpense.toString(), fontSize = 20.sp)
+                        Text(text = "30*", fontSize = 20.sp)
                     }
                 }
 
 
             }
 
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(vertical = 20.dp)
-            ) {
-                items(mainState.itemsList) { expenseEntity ->
-                    ExpenseCard(model = expenseEntity, modifier = Modifier.clickable {
-                        navController.navigate("${Screens.AddExpenseScreen.route}/${expenseEntity.id}")
-                    }, onDeleteClick = {
-                        viewModel.deleteExpenseNote(expenseEntity)
-                    })
-                }
-            }
+
         }
     }
 
