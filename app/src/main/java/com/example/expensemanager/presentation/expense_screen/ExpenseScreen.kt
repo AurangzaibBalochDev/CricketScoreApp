da91318b073d34656a9f8b550ea508d258427696
Aurangzaib BALOCH <zaibbuzdar3@gmail.com>
2024-08-19 07:08:45 -0700

    commit

--- app/src/main/java/com/example/expensemanager/presentation/expense_screen/ExpenseScreen.kt
+++ app/src/main/java/com/example/expensemanager/presentation/expense_screen/ExpenseScreen.kt
@@ -1,35 +1,22 @@
 package com.example.expensemanager.presentation.expense_screen
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
-import com.example.expensemanager.presentation.components.Screens
-import com.example.expensemanager.presentation.expense_card.ExpenseCard
-import com.example.expensemanager.presentation.main_screen.components.TopBar
 import com.example.expensemanager.ui.theme.CustomColors
 
 
@@ -64,7 +51,7 @@ fun ExpenseScreen(
                     ) {
 
                         Text(
-                            text = "Current Balance",
+                            text = "Total Scores",
                             color = Color.DarkGray,
                             modifier = Modifier.padding(vertical = 5.dp)
                         )
@@ -107,7 +94,7 @@ fun ExpenseScreen(
                     }
                     Row(
                     ) {
-                        Text(text = "30*", fontSize = 20.sp)
+                        Text(text = "30", fontSize = 20.sp)
                     }
                 }
 
